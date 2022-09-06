8cda230bdaa6e4ede2df36f2f74a005e5cf4598b
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2022-09-06 20:38:59 +0200

    limit to 10 dings per second

--- ding.sh
+++ ding.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 tty_bell() {
-    printf '\x07';
+    printf \\x07
 }
 
 phone() {
+    # thanks to https://osxdaily.com/2016/03/31/play-dtmf-tones-mac/
     afplay /System/Library/Components/CoreAudio.component/Contents/SharedSupport/SystemSounds/telephony/dtmf-pound.caf &
 }
 
+last_line="none"
+
 while read -r line;
 do
+    # limit to 10 dings per second
+    if [ "$last_line" = "${line:0:10}" ];
+    then
+        continue;
+    fi
+
+    last_line="${line:0:10}"
+
     #tty_bell
     phone
     printf .
